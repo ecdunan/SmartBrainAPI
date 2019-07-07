df0448eb448addb9ade884ffbf7123f3ecc69dd8
ecdunan <dunan.emmanuel@gmail.com>
2019-07-07 19:36:35 +0800

    Added validation for register and sign in

--- controllers/register.js
+++ controllers/register.js
@@ -2,28 +2,32 @@ const handleRegister = (db, bcrypt) => (req, res) => {
     const {name, email, password} = req.body;
     const hash = bcrypt.hashSync(password, 10);
 
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
+    if(!email || !password || !name) {
+        res.status(400).json('All fields must be populated.');
+    } else {
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        entries: 0,
+                        joined: new Date()
+                    })
+                    .then(user => res.json(user[0]))
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
         })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    name: name,
-                    email: loginEmail[0],
-                    entries: 0,
-                    joined: new Date()
-                })
-                .then(user => res.json(user[0]))
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'));
+        .catch(err => res.status(400).json('unable to register'))
+    }
 };
 
 module.exports =  {
